09e8b33790201587f9f61c88610247bb470000a0
Joao Victor Sena <jvsena16@gmail.com>
2025-10-05 16:15:44 -0300

    fix: remember selected nav item

--- app/src/main/java/com/github/jvsena42/floresta_node/presentation/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/github/jvsena42/floresta_node/presentation/ui/screens/main/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -66,7 +67,7 @@ private fun MainScreen(
     modifier: Modifier = Modifier,
     restartApplication: () -> Unit
 ) {
-    var navigationSelectedItem by remember { mutableStateOf(Destinations.NODE) }
+    var navigationSelectedItem by rememberSaveable { mutableStateOf(Destinations.NODE) }
     val navController = rememberNavController()
 
     Scaffold(
