8145571f927332fbe11d67be601b7a0118f0b9b6
Joao Victor Sena <jvsena16@gmail.com>
2026-04-30 09:34:30 -0300

    chore: bump version to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.github.jvsena42.mandacaru"
         minSdk = 29
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.6.0"
+        versionCode = 13
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
