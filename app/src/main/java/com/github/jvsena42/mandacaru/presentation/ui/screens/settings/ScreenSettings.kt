8a5364d57abacc9c9024937cf800a8c1d6b41e6a
TheButterZone <thebutterzone@cox.net>
2026-07-19 15:07:36 +0000

    add updateState smart-cast

--- app/src/main/java/com/github/jvsena42/mandacaru/presentation/ui/screens/settings/ScreenSettings.kt
+++ app/src/main/java/com/github/jvsena42/mandacaru/presentation/ui/screens/settings/ScreenSettings.kt
@@ -1143,10 +1143,10 @@ private fun UpdateRow(
                         }
                     }
             
-                    is UpdateState.Downloading -> { downloading ->
+                    is UpdateState.Downloading -> {
 
                         val animatedProgress by animateFloatAsState(
-                            targetValue = downloading.progress / 100f,
+                            targetValue = updateUiState.progress / 100f,
                             animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
                         )
 
@@ -1158,7 +1158,7 @@ private fun UpdateRow(
                         Spacer(modifier = Modifier.height(8.dp))
 
                         Text(
-                            text = "Downloading update… ${downloading.progress}%",
+                            text = "Downloading update… ${updateUiState.progress}%",
                             style = MaterialTheme.typography.bodySmall,
                         )
                     }
